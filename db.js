5e6b73cbde02bbe16d410565efa7b5e270deea13
Sravan-kumar04 <tiprisshravan@gmail.com>
2024-04-14 22:14:45 +0530

    ready to deploy

--- db.js
+++ db.js
@@ -7,11 +7,10 @@ const app = require("./index");
 const port = process.env.PORT || 5001;
 
 //  console.log(process.env);
- console.log(app.get('env'));
-console.log(process.env.MONGODB_URI)
+console.log(app.get("env"));
+console.log(process.env.MONGODB_URI);
 mongoose
-  .connect(process.env.MONGODB_URI, {
-  })
+  .connect(process.env.MONGODB_URI, {})
   .then((conn) => {
     // console.log(conn);
     console.log("DB connected successfully");
--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const cors = require("cors");
 // const connectToMongo = require("./db");
 // const dotenv = require("dotenv");
-// dotenv.config({ path: "/.env" }); 
+// dotenv.config({ path: "/.env" });
 
 const app = express();
 
 function logger(req, res, next) {
-    console.log(" logger middleware executed ");
-    next();
-  }
-
+  console.log(" logger middleware executed ");
+  next();
+}
 
 app.use(cors());
 app.use(express.json());
@@ -22,4 +21,4 @@ const Notes = require("./Routes/notes");
 app.use("/api/auth", Auth);
 app.use("/api/notes", Notes);
 
-module.exports=app;
+module.exports = app;
